ee60e0377b682954bdfd561fbb8d6df2f182fd6a
aletrujim <ale.trujim@hotmail.com>
2016-09-13 14:07:47 -0500

    commit 13/09

--- Webgescob/cuentasxcobrar.aspx.designer.cs
+++ Webgescob/cuentasxcobrar.aspx.designer.cs
@@ -110,5 +110,8 @@ namespace WebNif {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        protected global::System.Web.UI.WebControls.Label lblModalTitle;
+
     }
 }
